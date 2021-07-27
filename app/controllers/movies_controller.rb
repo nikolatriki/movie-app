c66c56d11f7ba3fe482b9ca171bfc5f46c43c912
nikolatriki <kjofte@yahoo.com>
2021-07-27 15:11:46 +0200

    Written new and create actions

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -6,4 +6,18 @@ class MoviesController < ApplicationController
   def show
     @movie = Movie.find(params[:id])
   end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+    @movie = Movie.new(title: '...', description: '....')
+    
+    if @movie.save
+      redirect_to @movie
+    else
+      render :new
+    end
+  end
 end
